ee3bd8246bd7ee94eb06c19de8c41f5f4c6e8a3f
Chanmoro <kazuki.m777@gmail.com>
2023-04-30 00:25:45 +0900

    Change tweepy to requests for twitter api access.

--- routes/oauth2_0.py
+++ routes/oauth2_0.py
@@ -1,13 +1,13 @@
 import base64
 import hashlib
+import json
 import os
 import random
 import string
 
-import orjson
-import tweepy
+import requests
 from flask import Blueprint, render_template, redirect, session, request, url_for
-from requests_oauthlib import OAuth2Session
+from requests_oauthlib import OAuth2, OAuth2Session
 
 oauth2_0_blueprint = Blueprint("oauth2_0", __name__, template_folder="templates")
 
@@ -15,38 +15,26 @@ oauth2_0_blueprint = Blueprint("oauth2_0", __name__, template_folder="templates"
 TWITTER_APP_CLIENT_ID = os.getenv("TWITTER_APP_CLIENT_ID")
 TWITTER_APP_CLIENT_SECRET = os.getenv("TWITTER_APP_CLIENT_SECRET")
 
-TWITTER_AUTH_URL = "https://twitter.com/i/oauth2/authorize"
-TWITTER_TOKEN_URL = "https://api.twitter.com/2/oauth2/token"
-
 
 @oauth2_0_blueprint.route("/")
 def index():
     """
     セッションに認証したユーザー情報、アクセストークンがある場合は内容を表示する
     """
-    oauth2_access_token = session.get("oauth2_access_token")
-    callback_args = session.get("callback_args")
-    oauth2_code_verifier = session.get("oauth2_code_verifier")
-    oauth2_state = session.get("oauth2_state")
-    authorized_user = session.get("authorized_user")
+    authorized_user_response = session.get("oauth2_authorized_user_response", {})
 
-    # datetime オブジェクトの json ダンプを簡単にするために orjson を利用
-    # fmt: off
     return render_template(
         "oauth2_0.html",
-        oauth2_access_token=orjson.dumps(
-            oauth2_access_token, option=orjson.OPT_APPEND_NEWLINE | orjson.OPT_INDENT_2
-        ).decode() if oauth2_access_token else None,
-        callback_args=orjson.dumps(
-            callback_args, option=orjson.OPT_APPEND_NEWLINE | orjson.OPT_INDENT_2
-        ).decode() if callback_args else None,
-        oauth2_code_verifier=oauth2_code_verifier,
-        oauth2_state=oauth2_state,
-        authorized_user=orjson.dumps(
-            authorized_user, option=orjson.OPT_APPEND_NEWLINE | orjson.OPT_INDENT_2
-        ).decode() if authorized_user else None,
+        oauth2_access_token=json.dumps(session.get("oauth2_access_token"), indent=2),
+        callback_args=json.dumps(session.get("oauth2_callback_args"), indent=2),
+        oauth2_code_verifier=session.get("oauth2_code_verifier"),
+        oauth2_state=session.get("oauth2_state"),
+        authorized_user=json.dumps(
+            authorized_user_response.get("body"), indent=2, ensure_ascii=False
+        ),
+        response_status=authorized_user_response.get("status_code"),
+        response_header=authorized_user_response.get("headers"),
     )
-    # fmt: on
 
 
 def generate_pkce_code() -> tuple[str, str]:
@@ -89,7 +77,9 @@ def twitter_auth():
     oauth2_session = create_oauth2_session()
     code_verifier, code_challenge = generate_pkce_code()
     authorization_url, state = oauth2_session.authorization_url(
-        TWITTER_AUTH_URL, code_challenge=code_challenge, code_challenge_method="S256"
+        "https://twitter.com/i/oauth2/authorize",
+        code_challenge=code_challenge,
+        code_challenge_method="S256",
     )
 
     session["oauth2_code_verifier"] = code_verifier
@@ -97,34 +87,46 @@ def twitter_auth():
     return redirect(authorization_url)
 
 
-def get_authorized_user(access_token: str) -> tweepy.user.User:
+def get_authorized_user(access_token: dict) -> dict:
     """
     twitter API v2 を利用して token に紐づく twitter アカウントの情報を取得する
     """
-    client = tweepy.Client(bearer_token=access_token)
-    # 取得したいユーザー情報のフィールドを指定するとレスポンスに追加される
-    # see: https://developer.twitter.com/en/docs/twitter-api/users/lookup/api-reference/get-users-me
-    response = client.get_me(
-        user_auth=False,
-        user_fields=[
-            "created_at",
-            "description",
-            "entities",
-            "id",
-            "location",
-            "name",
-            "pinned_tweet_id",
-            "profile_image_url",
-            "protected",
-            "public_metrics",
-            "url",
-            "username",
-            "verified",
-            "verified_type",
-            "withheld",
-        ],
+    oauth2 = OAuth2(
+        token={
+            "access_token": access_token["access_token"],
+            "token_type": access_token["token_type"],
+        }
     )
-    return response.data
+    res = requests.get(
+        "https://api.twitter.com/2/users/me",
+        auth=oauth2,
+        params={
+            "user.fields": ",".join(
+                [
+                    "created_at",
+                    "description",
+                    "entities",
+                    "id",
+                    "location",
+                    "name",
+                    "pinned_tweet_id",
+                    "profile_image_url",
+                    "protected",
+                    "public_metrics",
+                    "url",
+                    "username",
+                    "verified",
+                    "verified_type",
+                    "withheld",
+                ]
+            )
+        },
+    )
+    return {
+        "status_code": res.status_code,
+        "headers": "\n".join([f"{k}: {v}" for k, v in res.headers.items()]),
+        "body": res.json(),
+    }
 
 
 @oauth2_0_blueprint.route("/twitter_auth/callback")
@@ -133,7 +135,7 @@ def twitter_auth_callback():
     コールバックを処理する
     認可が正常に完了した場合はアクセストークンが取得できるので、トークンを利用して認可されたユーザーの情報を取得する
     """
-    session["callback_args"] = request.args
+    session["oauth2_callback_args"] = request.args
     # エラーがあった場合はクエリパラメータに error がセットされる
     error = request.args.get("error")
     if error:
@@ -142,16 +144,16 @@ def twitter_auth_callback():
     code = request.args.get("code")
     oauth2_session = create_oauth2_session(session["oauth2_state"])
     oauth2_access_token = oauth2_session.fetch_token(
-        token_url=TWITTER_TOKEN_URL,
+        token_url="https://api.twitter.com/2/oauth2/token",
         client_secret=TWITTER_APP_CLIENT_SECRET,
         code_verifier=session["oauth2_code_verifier"],
         code=code,
     )
 
     # 認可されたユーザー情報を取得する
-    authorized_user = get_authorized_user(oauth2_access_token["access_token"])
+    authorized_user_response = get_authorized_user(oauth2_access_token)
 
     # 簡易的にセッションにデータを保持するために dict に変換する
-    session["oauth2_access_token"] = dict(oauth2_access_token)
-    session["authorized_user"] = dict(authorized_user)
+    session["oauth2_access_token"] = oauth2_access_token
+    session["oauth2_authorized_user_response"] = authorized_user_response
     return redirect(url_for("oauth2_0.index"))
